5b90881453a8b38b936c3d329e346c55ead78d76
Alvaro Helton <alvarohelton@gmail.com>
2025-08-17 22:11:14 -0300

    Update hospedagens.js

--- scripts/hospedagens.js
+++ scripts/hospedagens.js
@@ -1,60 +1,60 @@
 function casafeliz(){
     document.getElementById("estabSelected").src = "img/pousadas/casafeliz.png";
-    document.getElementById("textRest").innerHTML= `Hello, World!  `
+    document.getElementById("textRest").innerHTML= `Descrição do estabelecimento`
 
 }
 function cabanas(){
     document.getElementById("estabSelected").src = "img/pousadas/cabanas.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 function enseada(){
     document.getElementById("estabSelected").src = "img/pousadas/enseada.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 function parracho(){
     document.getElementById("estabSelected").src = "img/pousadas/parracho.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 function pontadosaneis(){
     document.getElementById("estabSelected").src = "img/pousadas/pontadosaneis.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 function caseira(){
     document.getElementById("estabSelected").src = "img/pousadas/caseira.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 function paraiso(){
     document.getElementById("estabSelected").src = "img/pousadas/paraiso.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 
 function villa(){
     document.getElementById("estabSelected").src = "img/pousadas/villa.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 function oasis(){
     document.getElementById("estabSelected").src = "img/pousadas/oasis.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 function aldeia(){
     document.getElementById("estabSelected").src = "img/pousadas/aldeia.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 function bikibu(){
     document.getElementById("estabSelected").src = "img/pousadas/bikibu.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 
 }
 function cores(){
     document.getElementById("estabSelected").src = "img/pousadas/cores.png";
-    document.getElementById("textRest").innerHTML= `  `
+    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
 }
