68b841a69ae3f4ce2670456a7253821514f5bccf
Alvaro Helton <alvarohelton@gmail.com>
2025-09-02 19:38:09 -0300

    Add files via upload

--- scripts/hospedagens.js
+++ scripts/hospedagens.js
@@ -31,7 +31,13 @@ function parracho(){
 }
 function pontadosaneis(){
     document.getElementById("estabSelected").src = "img/pousadas/pontadosaneis.png";
-    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
+    document.getElementById("textRest").innerHTML= 
+    ` Na Pousada Ponta dos Anéis você se sente em casa, desfrutando a melhor localização da Praia de Maracajaú. 
+    Localizada na beira-mar, da famosa Praia de Maracajaú, ao lado do ponto de partida para os "Parrachos de Maracajaú". 
+    Com uma visual impressionante, oferecemos aos nossos hóspedes o que há de melhor para seu lazer, com muita descontração 
+    e conforto. O Mirante com uma grande piscina, para adultos e crianças, o restaurante com deliciosos pratos, preparados na hora. 
+    Uma grande área verde na frente dos apartamentos. Aguardamos vocês de braços abertos.
+    Fonte: <a target="_blank" href="http://pousadapontadosaneis.com.br/">pousadapontadosaneis.com.br</a>`
 
 }
 function caseira(){
@@ -47,12 +53,42 @@ function paraiso(){
 
 function villa(){
     document.getElementById("estabSelected").src = "img/pousadas/villa.png";
-    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
+    document.getElementById("textRest").innerHTML= 
+    ` Imagine um santuário onde a sofisticação se encontra com a beleza natural indomável. 
+    A Villa Mar-a-Villa oferece um refúgio exclusivo, um mundo à parte, mas com fácil acesso. 
+    A apenas uma hora da vibrante energia de Natal, capital do Rio Grande do Norte, 
+    e a apenas 45 minutos do Aeroporto Internacional, a Villa Mar-a-Villa proporciona um refúgio perfeito.
+    Aninhada nas praias ensolaradas perto da charmosa vila de pescadores de Maracajaú, 
+    nossa vila é um refúgio de tranquilidade. Aqui, dunas de areia infinitas encontram 
+    as águas cristalinas de recifes de corais protegidos, criando um playground para aventuras. 
+    Mergulhe com snorkel em meio à vibrante vida marinha, explore as dunas em um emocionante passeio de quadriciclo 
+    ou simplesmente perca-se nos quilômetros de praia intocada.
+    Dentro de um condomínio fechado e privativo - que já foi uma próspera plantação de coco -  
+    encontra-se o seu paraíso pessoal. Situada em uma extensa propriedade de 5.000 m², a vila é cercada 
+    por um jardim exuberante e pitoresco, repleto de flores nativas e coqueiros ondulantes.
+    A Villa Mar-a-Villa é mais do que um destino; é um convite para redescobrir a tranquilidade, 
+    abraçar o luxo e criar memórias inesquecíveis em um mundo de beleza incomparável.
+    Fonte: <a target="_blank" href="https://www.villa-mar-a-villa.com/about-villa-mar-a-villa">villa-mar-a-villa.com</a> `
 
 }
 function oasis(){
     document.getElementById("estabSelected").src = "img/pousadas/oasis.png";
-    document.getElementById("textRest").innerHTML= ` Descrição do estabelecimento `
+    document.getElementById("textRest").innerHTML= 
+    ` Estamos no litoral norte do Brasil, a 70km do aeroporto de Natal em um local tranquilo: Vila de pescadores Maracajaú.
+    Nas proximidades existem duas lagoas grandes e rasas que são muito divertidas para iniciantes e profissionais. 
+    O vento é sempre Right Cross Onshore. Aulas de kitesurf e aluguel de equipamentos de alta qualidade, 
+    como serviço de transporte para as lagoas e downwinders, estão disponíveis diariamente mediante solicitação.
+    Nosso Kitesurf Oasis possui dois quartos duplos e duas suítes luxuosas na villa. 
+    Além de outros dois quartos duplos exclusivos no chalé. Todos os quartos têm ar condicionado e são de última geração. 
+    Atribuímos grande importância à qualidade e limpeza. 
+    No jardim você encontrará diversas plantas tropicais e árvores frutíferas que ocasionalmente convidam macaquinhos ou iguanas 
+    a passar por aqui.Temos um restaurante no edifício principal onde a nossa chef Laila prepara iguarias locais 
+    e também pratos internacionais. Existe também um spa de massagens nas nossas instalações, 
+    para que nada falte depois de um dia cansativo na água. Há também uma confortável plataforma de observação 
+    com uma excelente vista sobre a praia de mar azul-turquesa e uma piscina para se refrescar. 
+    Existem vistas para o mar da nossa propriedade. A área fica a menos de um minuto da praia e do centro de kite local de Maracajaú.
+    Temos vento o ano todo! O vento térmico geralmente chega a partir do meio-dia.
+    Fonte: <a target="_blank" href="https://www.kitesurf-oasis-maracajau.com/pt">kitesurf-oasis-maracajau.com</a> `
 
 }
 function aldeia(){
